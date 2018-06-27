7c7f50a382bcd7520f0eb61be2b225b51771a495
maylaizco <37838259+maylaizco@users.noreply.github.com>
2018-06-27 07:42:24 -0300

    Add files via upload

---
+++ P6.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int sm(int len, int *v)
+
+{
+
+   if(len>0)
+
+        return *v+sm(len-1, v+1);
+
+    else
+
+        return 0.0;
+
+}
+
+float med(int x, int y)
+
+{
+
+    return (float)x/(float)y;
+
+}
+
+int main ()
+
+{
+
+	int vet[]={0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+
+	int tam=0, i=0;
+
+	printf("Quantidade a ser somada:\n");
+
+	scanf("%d", &tam);
+
+	printf("\nValores: \n");
+
+	for (i=0; i<tam; i++)
+ 
+		scanf("%d", &vet[i]);
+
+	printf("\nmedia = %f", med(sm(tam, vet), tam));
+
+	return 0;
+
+}
