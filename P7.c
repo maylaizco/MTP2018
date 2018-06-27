974c24df6e33d286fdffc87b4e06bc046ea30954
maylaizco <37838259+maylaizco@users.noreply.github.com>
2018-06-27 07:46:00 -0300

    Add files via upload

---
+++ P7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int A(int m, int n)
+
+{
+
+    if (m==0)
+
+        return n+1;
+
+    else if (m>0 && n==0)
+
+        return A(m-1, 1);
+
+    else if (m>0 && n>0)
+
+        return A(m-1, A(m, n-1));
+
+}
+
+int main ()
+
+{
+
+	int n=0, m=0;
+
+	printf("Valores para n e m:\n");
+
+	scanf("%d %d", &m, &n);
+
+	printf("%d", A(m, n));
+
+	return 0;
+
+}
